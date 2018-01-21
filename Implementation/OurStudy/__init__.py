f88589590e805309ba3074392f33975950e4952e
LoicEm <loic.macherel@gmail.com>
2018-01-21 16:21:05 +0100

    added ipython notebook and dependencies in the 'ourstudy' folder

---
+++ Implementation/OurStudy/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+__init__.py file for ipt package
+Bryan S. Graham, UC - Berkeley
+bgraham@econ.berkeley.edu
+16 May 2016
+"""
+
+# Import the different functions into the package
+#from ols import ols
+from logit import logit
+from att import att
